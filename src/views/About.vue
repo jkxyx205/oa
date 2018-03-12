c52b549e98a678d28a70595e5c4f41dee06303d8
Rick <jkxyx205@163.com>
2018-03-12 15:01:55 +0800

    initial vue-cli && ElementUI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page,I use ElementUI blow:</h1>
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+    <hr/>
+
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+  </el-tabs>
   </div>
 </template>
+
+<script>
+    import Vue from 'vue';
+    import ElementUI from 'element-ui';
+    import 'element-ui/lib/theme-chalk/index.css';
+    Vue.use(ElementUI);
+
+    export default {
+        data () {
+          return {
+            radio: '2',
+            activeName: 'second'
+          };
+        },
+        methods: {
+          handleClick(tab, event) {
+            console.log(tab, event);
+          }
+        }
+    }
+</script>
